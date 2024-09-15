7a6557e7eb46f5b4d8e0a13d5513924597126f6a
Fabio <fabio.waljaard@gmail.com>
2024-09-15 19:08:15 +0000

    Properly close API when unloading

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 """The YouTube Lounge integration."""
+
 from __future__ import annotations
 
 from pyytlounge import YtLoungeApi
@@ -44,7 +45,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        api = hass.data[DOMAIN].pop(entry.entry_id)
-        api.close()
+        api: YtLoungeApi = hass.data[DOMAIN].pop(entry.entry_id)
+        await api.close()
 
     return unload_ok
